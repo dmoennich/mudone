fe9092919a38864171696df80b1fda9155ea244d
Daniel Moennich <daniel.moennich@swisscom.com>
2017-05-21 10:48:51 +0200

    Start conversation processing.

--- input-handler.js
+++ input-handler.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const RoomService = require('./room-service');
 
 class InputHandler {
@@ -18,7 +19,8 @@ class InputHandler {
                             .map(u => u.getName() === user.getName() ? user.getName() + ' (you)' : u.getName())
                             .join(', ')
                     );
-                case 'start-conversation': //TODO check against users
+                case 'start-conversation':
+                    return this.processStartConversation(user, io, socket, response);
                 case 'input.unknown':
                 case 'welcome':
                     return socket.emit('chat message', response.result.fulfillment.speech);
@@ -26,6 +28,16 @@ class InputHandler {
                     return socket.emit('chat message', 'hm...');
             }
         });
+    }
+
+    processStartConversation(user, io, socket, response) {
+        const desiredConversationTarget = response.result.parameters.conversationTarget;
+        const desiredConversationTargetLc = desiredConversationTarget && desiredConversationTarget.toLowerCase();
+        const actualConversationTarget = _.find(this.room.getUsers(), u => u.getName().toLowerCase() === desiredConversationTargetLc);
+        if (actualConversationTarget) {
+            return socket.emit('chat message', response.result.fulfillment.speech);
+        }
+        return socket.emit('chat message', `Sorry, there is noone here named ${desiredConversationTarget}`);
     }
 
 }
